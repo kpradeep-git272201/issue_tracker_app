d691bb2a68aa856d3736a4b1715e92f94094d84e
Suraj Kumar Mishra <surajmishra9318@gmail.com>
2025-05-08 10:55:35 +0530

    test

--- src/app/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { MaterialModule } from '../../../material/material.module';
 import { WidgetCardComponent } from '../widget-card/widget-card.component';
 import { SharedModule } from '../../../shared/shared.module';
@@ -6,17 +7,37 @@ import { CommonService } from '../../../services/common.service';
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
   imports: [MaterialModule, WidgetCardComponent, SharedModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  breakpoint: number = 3;
+  isBrowser: boolean = false;
 
-  constructor(private commonService: CommonService) {
-
+  constructor(
+    private commonService: CommonService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
   }
 
   ngOnInit() {
+    if (this.isBrowser) {
+      this.setGridBreakpoint(window.innerWidth);
+    }
+  }
 
+  onResize(event: any) {
+    if (this.isBrowser) {
+      this.setGridBreakpoint(event.target.innerWidth);
+    }
+  }
+
+  private setGridBreakpoint(width: number) {
+    this.breakpoint = width <= 768 ? 1 : 2;
   }
 }
+
+//TEST1

71a2e0ba3d5a6de6edef6b9f61b798bab21ea6f1
kpradeep-git272201 <kpradeep.git@gmail.com>
2025-05-23 15:58:15 +0530

    pending

--- src/app/services/json/theme/themelist.service.ts
+++ src/app/services/json/theme/themelist.service.ts
@@ -12,39 +12,39 @@ export class ThemelistService {
       filterData: [
         {
           code: 1,
-          vlaue: 'Theme 1 - Poverty Free and Enhanced Livelihoods Village',
+          value: 'Theme 1 - Poverty Free and Enhanced Livelihoods Village',
         },
         {
           code: 2,
-          vlaue: 'Theme 2 - Healthy Village',
+          value: 'Theme 2 - Healthy Village',
         },
         {
           code: 3,
-          vlaue: 'Theme 3 - Child Friendly Village',
+          value: 'Theme 3 - Child Friendly Village',
         },
         {
           code: 4,
-          vlaue: 'Theme 4 - Water Sufficient Village',
+          value: 'Theme 4 - Water Sufficient Village',
         },
         {
           code: 5,
-          vlaue: 'Theme 5 - Clean and Green Village',
+          value: 'Theme 5 - Clean and Green Village',
         },
         {
           code: 6,
-          vlaue: 'Theme 6 - Self-sufficient Infrastructure in Village',
+          value: 'Theme 6 - Self-sufficient Infrastructure in Village',
         },
         {
           code: 7,
-          vlaue: 'Theme 7 - Socially Just and Socially Secured Village',
+          value: 'Theme 7 - Socially Just and Socially Secured Village',
         },
         {
           code: 8,
-          vlaue: 'Theme 8 - Village with Good Governance',
+          value: 'Theme 8 - Village with Good Governance',
         },
         {
           code: 9,
-          vlaue: 'Theme 9 - Women Friendly Village',
+          value: 'Theme 9 - Women Friendly Village',
         }
       ]
     }

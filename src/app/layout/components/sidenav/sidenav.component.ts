546fe6f4980b560f9ec42382f5e172ae931afd29
shreya <jainparas1034@gmail.com>
2025-05-26 16:41:47 +0530

    amchart-responsive

--- src/app/layout/components/sidenav/sidenav.component.ts
+++ src/app/layout/components/sidenav/sidenav.component.ts
@@ -13,7 +13,7 @@ import { ProjectStatusComponent } from '../sidefilters/project-status/project-st
 import { DevelopementCategoryComponent } from '../sidefilters/developement-category/developement-category.component';
 import { GeographicalFilterComponent } from '../sidefilters/geographical-filter/geographical-filter.component';
 import { DashboardFilterComponent } from '../sidefilters/dashboard-filter/dashboard-filter.component';
-
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-sidenav',
   imports: [
@@ -66,6 +66,14 @@ export class SidenavComponent {
     // });
   }
 
+  exportExcel() {
+    const fileName = "ExcelSheet.xlsx";
+    let data = document.getElementById("table-data");
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(data);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    XLSX.writeFile(wb, fileName);
+  }
 
   
 }

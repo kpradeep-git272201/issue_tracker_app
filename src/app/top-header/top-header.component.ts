a6c512f8847659350fcd8affe6ebd6b11268dda4
kpradeep-git272201 <kpradeep.git@gmail.com>
2025-05-29 17:40:48 +0530

    LOGOUT

--- src/app/top-header/top-header.component.ts
+++ src/app/top-header/top-header.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../material/material.module';
+import { LoginComponent } from '../admin/components/login/login.component';
+import { MatDialog } from '@angular/material/dialog';
+import { AuthService } from '../services/planning/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-top-header',
   imports: [MaterialModule],
@@ -7,5 +11,52 @@ import { MaterialModule } from '../material/material.module';
   styleUrl: './top-header.component.scss'
 })
 export class TopHeaderComponent {
+isLoggedIn: boolean | undefined;
 
+  constructor(private dialog: MatDialog,
+    private authService: AuthService,
+    private router: Router
+  ) {
+
+  }
+
+  ngOnInit(){
+    this.authService.isLoggedIn$.subscribe((status) => {
+      this.isLoggedIn = status;
+    });
+  }
+  getLogin() {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      disableClose: true,
+      hasBackdrop: true,
+      width: '450px',
+      panelClass: 'custom-login-dialog',
+      data: {
+        title: 'Login',
+      },
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('Dialog closed. Received:', result);
+    });
+  }
+
+  closeDialog(){
+
+  }
+
+  getDialog(action:string){
+    if(action=='Notification'){
+
+    }else if(action=='Settings'){
+
+    }
+  }
+
+  logout(){
+    this.authService.logout();
+    this.isLoggedIn=this.authService.isAuthenticated();
+    if(!this.isLoggedIn){
+      this.router.navigate(["/"])
+    }
+  }
 }

1420d97b2b8b038cb654efbbb0afe2da206a86a3
kpradeep-git272201 <kpradeep.git@gmail.com>
2025-05-29 17:18:32 +0530

    topheader

--- src/app/restricted/layout/main/main.component.ts
+++ src/app/restricted/layout/main/main.component.ts
@@ -4,10 +4,11 @@ import { SubHeaderComponent } from "../../../public/layout/sub-header/sub-header
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { MenuComponent } from "../menu/menu.component";
+import { TopHeaderComponent } from "../../../top-header/top-header.component";
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, SubHeaderComponent, MenuComponent],
+  imports: [HeaderComponent, SubHeaderComponent, MenuComponent, TopHeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })

888f02de1a9b01fd4dac6d472963b4f86cd225bd
kpradeep-git272201 <kpradeep.git@gmail.com>
2025-05-30 16:29:48 +0530

    login logout functionality done by local data

--- src/app/restricted/layout/header/header.component.ts
+++ src/app/restricted/layout/header/header.component.ts
@@ -1,35 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { MaterialModule } from '../../../material/material.module';
 import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from '../../../services/planning/auth.service';
 import { LoginComponent } from '../../../admin/components/login/login.component';
 import { Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   imports: [MaterialModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-isLoggedIn: boolean | undefined;
+  isLoggedIn: boolean | undefined;
   loggedUser: any;
 
-  constructor(private dialog: MatDialog,
+  constructor(
+    private dialog: MatDialog,
     private authService: AuthService,
-    private router: Router
-  ) {
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object,
+  ) {}
 
-  }
-
-  ngOnInit(){
+  ngOnInit() {
     this.authService.isLoggedIn$.subscribe((status) => {
       this.isLoggedIn = status;
     });
 
-    const userString = localStorage.getItem('userJson');
-    if(userString){
-      this.loggedUser=JSON.parse(userString);
+    if (isPlatformBrowser(this.platformId)) {
+      const userString = localStorage.getItem('userJson');
+      if (userString) {
+        this.loggedUser = JSON.parse(userString).data;
+      }
     }
   }
   getLogin() {
@@ -42,28 +45,24 @@ isLoggedIn: boolean | undefined;
         title: 'Login',
       },
     });
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       console.log('Dialog closed. Received:', result);
     });
   }
 
-  closeDialog(){
+  closeDialog() {}
 
-  }
-
-  getDialog(action:string){
-    if(action=='Notification'){
-
-    }else if(action=='Settings'){
-
+  getDialog(action: string) {
+    if (action == 'Notification') {
+    } else if (action == 'Settings') {
     }
   }
 
-  logout(){
+  logout() {
     this.authService.logout();
-    this.isLoggedIn=this.authService.isAuthenticated();
-    if(!this.isLoggedIn){
-      this.router.navigate(["/"])
+    this.isLoggedIn = this.authService.isAuthenticated();
+    if (!this.isLoggedIn) {
+      this.router.navigate(['/']);
     }
   }
 }

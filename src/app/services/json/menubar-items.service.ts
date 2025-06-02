7e9655b3b83764e64a1621eedf0dcaf4d7130628
Suraj Kumar Mishra <surajmishra9318@gmail.com>
2025-06-02 11:20:55 +0530

    sidebar dynamic json partially done

--- src/app/services/json/menubar-items.service.ts
+++ src/app/services/json/menubar-items.service.ts
@@ -10,270 +10,529 @@ export class MenubarItemsService {
   getPanelItems() {
     return [
       {
-        title: 'Meri Panchayat',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
+          "id": 473,
+          "name": "Bank Account Management",
+          "url": null,
+          "subMenus": [
+              {
+                  "id": 214,
+                  "name": "menu.BankDetail",
+                  "url": null,
+                  "subMenus": [
+                      {
+                          "id": 215,
+                          "name": "Label.Add",
+                          "url": "addBankDetails",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 216,
+                          "name": "Label.Manage",
+                          "url": "manageBankDetails",
+                          "subMenus": []
+                      }
+                  ]
+              },
+              {
+                  "id": 226,
+                  "name": "Label.treasury",
+                  "url": null,
+                  "subMenus": [
+                      {
+                          "id": 227,
+                          "name": "Label.Add",
+                          "url": "addTreasury",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 228,
+                          "name": "Label.Manage",
+                          "url": "manageTreasury",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 229,
+                          "name": "Label.maps",
+                          "url": "mapTreasury12",
+                          "subMenus": []
+                      }
+                  ]
+              },
+              {
+                  "id": 230,
+                  "name": "Label.ManagePDA",
+                  "url": null,
+                  "subMenus": [
+                      {
+                          "id": 231,
+                          "name": "Label.Add",
+                          "url": "addTreasuryAccount",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 232,
+                          "name": "Label.Manage",
+                          "url": "manageTreasuryAccount",
+                          "subMenus": []
+                      }
+                  ]
+              },
+              {
+                  "id": 233,
+                  "name": "label.branchManagement",
+                  "url": null,
+                  "subMenus": [
+                      {
+                          "id": 234,
+                          "name": "Label.Add",
+                          "url": "addBankBranch",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 235,
+                          "name": "Label.modify",
+                          "url": "updateBankBranch",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 236,
+                          "name": "Label.maps",
+                          "url": "mapBankBranch",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 315,
+                          "name": "Label.view",
+                          "url": "viewBankBranch",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 316,
+                          "name": "Label.delete",
+                          "url": "deleteBankBranch",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 354,
+                          "name": "menu.ViewBranchDetails",
+                          "url": "branchDetailsReport",
+                          "subMenus": []
+                      }
+                  ]
+              },
+              {
+                  "id": 237,
+                  "name": "Label.BankAccount",
+                  "url": null,
+                  "subMenus": [
+                      {
+                          "id": 238,
+                          "name": "Label.Add",
+                          "url": "addBankAccount",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 239,
+                          "name": "Label.Manage",
+                          "url": "manageBankAccount",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 240,
+                          "name": "menu.ViewPFMSStatus",
+                          "url": "viewPFMSBankDetails",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 353,
+                          "name": "menu.ViewEgramPFMSDetails",
+                          "url": "accountDetails",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 402,
+                          "name": "menu.DisableAccount",
+                          "url": "getduplicateAccount",
+                          "subMenus": []
+                      }
+                  ]
+              },
+              {
+                  "id": 241,
+                  "name": "Label.po",
+                  "url": null,
+                  "subMenus": [
+                      {
+                          "id": 242,
+                          "name": "Label.Add",
+                          "url": "addPostOffice",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 243,
+                          "name": "Label.Manage",
+                          "url": "managePostOffice",
+                          "subMenus": []
+                      }
+                  ]
+              },
+              {
+                  "id": 244,
+                  "name": "label.managePOAccount",
+                  "url": null,
+                  "subMenus": [
+                      {
+                          "id": 245,
+                          "name": "Label.Add",
+                          "url": "addPostOfficeAccount",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 246,
+                          "name": "Label.Manage",
+                          "url": "managePostOfficeAccount",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 247,
+                          "name": "menu.ModifyStatus",
+                          "url": "modifyStatusPostOfficeAccount",
+                          "subMenus": []
+                      }
+                  ]
+              },
+              {
+                  "id": 248,
+                  "name": "Label.chequeBook",
+                  "url": null,
+                  "subMenus": [
+                      {
+                          "id": 249,
+                          "name": "Label.Add",
+                          "url": "addChequeBook",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 250,
+                          "name": "Label.Manage",
+                          "url": "manageChequeBook",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 251,
+                          "name": "menu.ModifyStatus",
+                          "url": "modifyChequeBookleafsStatus",
+                          "subMenus": []
+                      }
+                  ]
+              }
+          ]
       },
       {
-        title: 'Profile Management',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/profile-management/add'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-      {
-        title: 'PESA Profiler',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-      {
-        title: 'Funds / Resource Envelope',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-
-      {
-        title: 'Panchayat Development Plan',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-
-      {
-        title: 'Works/Progress Management',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-      {
-        title: 'Bank Account Management',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-      {
-        title: 'Master Data Management',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-
-      {
-        title: 'Vendors Management',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-
-      {
-        title: 'Voucher/E-Payment Management',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-      {
-        title: 'Accounting Management',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-      {
-        title: 'Assets Register',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-      {
-        title: 'mActionSoft',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-      {
-        title: 'Panchayat Window',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-
-      {
-        title: 'Weather Forecast',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-
-      {
-        title: 'Internet Connection',
-        icon: 'SidebarIcon',
-        options: [
-          {
-            label: 'Add',
-            icon: 'AddIcon',
-            route: '/'
-          },
-          {
-            label: 'View',
-            icon: 'Visibility',
-            route: '/'
-          }
-        ]
-      },
-    ];
+          "id": 474,
+          "name": "Vendors Management",
+          "url": null,
+          "subMenus": [
+              {
+                  "id": 252,
+                  "name": "Label.Beneficiary",
+                  "url": null,
+                  "subMenus": [
+                      {
+                          "id": 253,
+                          "name": "Label.Add",
+                          "url": "getAddBenificary",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 254,
+                          "name": "Label.Manage",
+                          "url": "getModifyBenificary",
+                          "subMenus": []
+                      },
+                      {
+                          "id": 313,
+                          "name": "Label.approve",
+                          "url": "approveBenificary",
+                          "subMenus": []
+                      }
+                  ]
+              }
+          ]
+      }
+    ]
   }
 
 
 }
+
+
+
+
+// [
+//   {
+//     title: 'Meri Panchayat',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+//   {
+//     title: 'Profile Management',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/profile-management/add'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+//   {
+//     title: 'PESA Profiler',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+//   {
+//     title: 'Funds / Resource Envelope',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+
+//   {
+//     title: 'Panchayat Development Plan',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+
+//   {
+//     title: 'Works/Progress Management',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+//   {
+//     title: 'Bank Account Management',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+//   {
+//     title: 'Master Data Management',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+
+//   {
+//     title: 'Vendors Management',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+
+//   {
+//     title: 'Voucher/E-Payment Management',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+//   {
+//     title: 'Accounting Management',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+//   {
+//     title: 'Assets Register',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+//   {
+//     title: 'mActionSoft',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+//   {
+//     title: 'Panchayat Window',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+
+//   {
+//     title: 'Weather Forecast',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+
+//   {
+//     title: 'Internet Connection',
+//     icon: 'SidebarIcon',
+//     options: [
+//       {
+//         label: 'Add',
+//         icon: 'AddIcon',
+//         route: '/'
+//       },
+//       {
+//         label: 'View',
+//         icon: 'Visibility',
+//         route: '/'
+//       }
+//     ]
+//   },
+// ];
+
+
 

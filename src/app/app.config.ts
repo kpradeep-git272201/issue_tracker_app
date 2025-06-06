0365e97a7b576e10236d2f3d9a75850da382ed3a
kpradeep-git272201 <kpradeep.git@gmail.com>
2025-06-06 12:19:23 +0530

    inprogress

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,14 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    importProvidersFrom({
-      TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: TranslateLoader,
-        deps: [HttpClient]
-      },
-    }),
-    }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withInterceptorsFormDi()),
+    provideHttpClient(),
   ]
 };
--- src/app/top-header/top-header.component.ts
+++ src/app/top-header/top-header.component.ts
@@ -1,31 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MaterialModule } from '../material/material.module';
 import { LoginComponent } from '../admin/components/login/login.component';
 import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from '../services/planning/auth.service';
 import { Router } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-top-header',
-  imports: [MaterialModule],
+  imports: [MaterialModule, TranslateModule],
   templateUrl: './top-header.component.html',
-  styleUrl: './top-header.component.scss'
+  styleUrl: './top-header.component.scss',
 })
 export class TopHeaderComponent {
-isLoggedIn: boolean | undefined;
+  isLoggedIn: boolean | undefined;
+  translate: TranslateService = inject(TranslateService);
 
-  constructor(private dialog: MatDialog,
+  constructor(
+    private dialog: MatDialog,
     private authService: AuthService,
-    private router: Router
-  ) {
+    private router: Router,
+  ) {}
 
-  }
-
-  ngOnInit(){
+  ngOnInit() {
     /** this.authService.isLoggedIn$.subscribe((status) => {
       this.isLoggedIn = status;
     }); */
-    this.isLoggedIn=this.authService.isAuthenticated();
-    
+    this.isLoggedIn = this.authService.isAuthenticated();
   }
   getLogin() {
     const dialogRef = this.dialog.open(LoginComponent, {
@@ -37,28 +37,24 @@ isLoggedIn: boolean | undefined;
         title: 'Login',
       },
     });
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       console.log('Dialog closed. Received:', result);
     });
   }
 
-  closeDialog(){
+  closeDialog() {}
 
-  }
-
-  getDialog(action:string){
-    if(action=='Notification'){
-
-    }else if(action=='Settings'){
-
+  getDialog(action: string) {
+    if (action == 'Notification') {
+    } else if (action == 'Settings') {
     }
   }
 
-  logout(){
+  logout() {
     this.authService.logout();
-    this.isLoggedIn=this.authService.isAuthenticated();
-    if(!this.isLoggedIn){
-      this.router.navigate(["/"])
+    this.isLoggedIn = this.authService.isAuthenticated();
+    if (!this.isLoggedIn) {
+      this.router.navigate(['/']);
     }
   }
 }

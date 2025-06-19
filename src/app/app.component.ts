9af0d6bb1a01628a4c674341e834c75dadf5b548
kpradeep-git272201 <kpradeep.git@gmail.com>
2025-06-19 14:48:47 +0530

    inprogress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
-import { AfterViewInit, Component, inject, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  inject,
+  Inject,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
 import { SidenavComponent } from './layout/components/sidenav/sidenav.component';
 import { MainHeaderComponent } from './layout/components/main-header/main-header.component';
 import { IconsService } from './services/icons/icons.service';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { FooterComponent } from './layout/components/footer/footer.component';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
 import { MaterialModule } from './material/material.module';
 import { TranslateService } from './services/language/translate.service';
 import { ThemeService } from './services/themes/theme.service';
-
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,19 +25,20 @@ import { ThemeService } from './services/themes/theme.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit, AfterViewInit{
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'integrated_dashboard';
   opened: any = true;
   translate: TranslateService = inject(TranslateService);
+  private router = inject(Router);
   constructor(
     private iconService: IconsService,
     private iconRegistry: MatIconRegistry,
     private sanitizer: DomSanitizer,
     private translateService: TranslateService,
     @Inject(PLATFORM_ID) private platformId: Object,
-    private themeService: ThemeService
+    private themeService: ThemeService,
   ) {
-      this.themeService.loadTheme();
+    this.themeService.loadTheme();
     this.iconService.getIconName().forEach((icon) => {
       this.iconRegistry.addSvgIcon(
         icon,
@@ -40,7 +48,11 @@ export class AppComponent implements OnInit, AfterViewInit{
   }
 
   ngOnInit() {
- 
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        localStorage.setItem('lastVisitedRoute', event.urlAfterRedirects);
+      });
   }
   ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
@@ -49,7 +61,6 @@ export class AppComponent implements OnInit, AfterViewInit{
     }
   }
 
-  
   getOpenedStat(event: any) {
     this.opened = event;
   }
--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,35 +1,16 @@
-// import { CanActivateFn } from '@angular/router';
-
-// export const authGuard: CanActivateFn = (route, state) => {
-//   return true;
-// };
-
-import { CanActivate, CanActivateFn, UrlTree } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../services/planning/auth.service';
-import { Router } from 'express';
-import { Inject, PLATFORM_ID } from '@angular/core';
-import { Observable } from 'rxjs';
-import { isPlatformBrowser } from '@angular/common';
-
-export class AuthGuard implements CanActivate {
-
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-    @Inject(PLATFORM_ID) private platformId: Object
-  ) {}
-
-  canActivate():
-    | boolean
-    | UrlTree
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree> {
-    
-    if (isPlatformBrowser(this.platformId) && this.authService.isAuthenticated()) {
-      return true;
-    } else {
-      // Redirect to demo-m or login
-      return false; //this.router.parseUrl('/');
-    }
+import { Router } from '@angular/router';
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const isAuthenticated = authService.isAuthenticated();
+  if (isAuthenticated) {
+    return true;
+  } else {
+    router.navigate(['/login']);
+    return false;
   }
-}
+};
+
